8101557327867aeeefbd5de69bde49da1cdacdd9
ArthurDiduch <arthurdiduch@gmail.com>
2022-09-09 07:47:52 -0300

    Genre and book created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeOrm.config';
 import { PersonModule } from './person/person.module';
+import { GenreModule } from './genre/genre.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PersonModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PersonModule, GenreModule, BookModule],
 })
 export class AppModule {}
