4fc9686089ef329a165dc0d9bda5917e01fa3379
gabriel milosz cruz <gbrlmilosz@gmail.com>
2022-09-09 07:43:11 -0300

    create employee

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeOrm.config';
 import { PersonModule } from './person/person.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PersonModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PersonModule, EmployeeModule],
 })
 export class AppModule {}
