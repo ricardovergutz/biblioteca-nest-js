a921b07260d6cba12021567b89294915de30ec8e
José Maria de Mattos Netto <mariaze214@gmail.com>
2022-09-13 09:48:24 -0300

    Delete e Update
    Foi ajustado o delete e o update do person, para usar a relação tambem.

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -3,18 +3,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PersonDto } from './person.dto';
 import { PersonEntity } from './person.entity';
+import { EmployeeService } from './employee/employ.service';
 
 @Injectable()
 export class PersonService {
   constructor(@InjectRepository(PersonEntity)
-              private personRepository: Repository <PersonEntity>){}
+              private personRepository: Repository <PersonEntity>,               
+              private employeeService: EmployeeService){}
   
   async showAll(){
     return await this.personRepository.find();
   }
 
-  async findOne(id:number){
-    return await this.personRepository.findOne({where: {id}})
+  async findOne(id:number, employee:boolean=false){
+    const user = await this.personRepository.findOne({where: {id}, relations: { employee: employee,  }, });
+    if(!user){
+      throw new NotFoundException();
+    }
+    return user;
   }
 
   async createPerson(person: PersonDto): Promise<PersonEntity>{
@@ -23,17 +29,16 @@ export class PersonService {
   }
 
   async update(id: number, person:PersonDto){
-    const user = await this.personRepository.findOne({where: {id: id}});
-    if(!user){
-      throw new NotFoundException();
-    }
-    await this.personRepository.update({id}, person);
-    return person;
+    await this.personRepository.update(id, person);
+    return await this.findOne(id);
   }
 
   async destroy(id:number){
-    const data = await this.personRepository.findOne({where: {id}})
-    await this.personRepository.delete(id)
-    return data
+    const person = await this.findOne(id, true);
+    if (person.employee){
+      await this.employeeService.destroy(person.employee.id);
+    }
+    await this.personRepository.delete(id);
+    return true;
   }
 }
