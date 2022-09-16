42539179314517ac13e9a58ef940c12e28a3cbb1
Ricardo Vergutz <ricardovergutz@gmail.com>
2022-09-16 00:19:22 -0300

    Correção livro exempo 1

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,10 +3,12 @@ import { BookService } from './book.service';
 import { BookController } from './book.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
+import { GenreService } from 'src/genre/genre.service';
+import { Genre } from 'src/genre/entities/genre.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book, Genre])],
   controllers: [BookController],
-  providers: [BookService]
+  providers: [BookService, GenreService],
 })
 export class BookModule {}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { booksAuthorscontroller } from 'src/authors/books_authors.controller';
+import { Genre } from 'src/genre/entities/genre.entity';
 import { Repository } from 'typeorm';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -9,16 +11,20 @@ import { Book } from './entities/book.entity';
 @Injectable()
 export class BookService {
   constructor(
-    @InjectRepository(Book) private bookRepository: Repository<Book>
+    @InjectRepository(Book) private bookRepository: Repository<Book>,
+    @InjectRepository(Genre) private genreRepository: Repository<Genre>
   ){}
 
   async create(createBookDto: CreateBookDto): Promise<Book> {
-    const book = new Book();
+    let genre = new Genre();
+    genre.name = 'teste';
+    await this.genreRepository.save(genre);
+
+    let book = new Book();
     book.name = createBookDto.name;
-    book.url = createBookDto.url;
-    book.genre = createBookDto.genreId;
-    
-    return await this.bookRepository.save(createBookDto);
+    book.url = createBookDto.url
+    book.genre = genre;
+    return await this.bookRepository.save(book);
   }
 
   async findAll() {
