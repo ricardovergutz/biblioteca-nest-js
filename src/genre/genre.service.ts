a20f7b89deda1ae4a00357c2e697e5709e0f735d
ArthurDiduch <arthurdiduch@gmail.com>
2022-09-19 12:23:27 -0300

    Regras de negocio genre

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateGenreDto } from './dto/create-genre.dto';
@@ -12,7 +12,11 @@ export class GenreService {
   ) {}
 
   async create(createGenreDto: CreateGenreDto): Promise<Genre> {
+    try {
     return await this.genreRepository.save(createGenreDto);
+    } catch(err) {
+      throw new ConflictException({message: "Genero  já existente"});
+    }
   }
 
   async findAll(): Promise<Genre[]> {
@@ -20,12 +24,16 @@ export class GenreService {
   }
 
   async findOne(id: number) {
-    return await this.genreRepository.findOne({ where: { id: id } });
+    return await this.genreRepository.findOne({ where: { id } });
   }
 
   async update(id: number, updateGenreDto: UpdateGenreDto) {
+    try {
     await this.genreRepository.update({ id }, updateGenreDto);
     return await this.genreRepository.findOne({ where: { id: id } });
+    } catch(err) {
+      throw new ConflictException({message: "Genero  já existente"});
+    }
   }
 
   async remove(id: number) {
