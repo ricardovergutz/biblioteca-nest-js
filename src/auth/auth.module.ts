9be7b226c44ef2238a3668ef8ed5f9a12f19fe0d
gabriel milosz cruz <gbrlmilosz@gmail.com>
2022-09-20 12:13:22 -0300

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { PersonModule } from 'src/person/person.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports
+  imports: [PersonModule, PassportModule,
+  JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: {expiresIn: '30d'}
+  })],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, /* LocalStrategy, JwtStrategy */]
 })
-export class AuthModule {}
+export class AuthModule /* implements NestModule */ {
+  /* configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  } */
+}
